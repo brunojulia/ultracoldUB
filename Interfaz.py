2c8f270b4ae484c633e7342ffb0910fce3d38f96
IvanMorera <imorerna7@alumnes.ub.edu>
2016-10-06 23:44:17 +0200

    Interfaz para el wavepack dispersion
    Subo la interfaz y los cambios en el código para adaptarlo a la interfaz. Falta bastante para hacer en la interfaz, esto solo es una demo.

--- Interfaz.py
+++ Interfaz.py
@@ -424,7 +424,248 @@ class WD(QMainWindow,Ui_MainWindow):
     def __init__(self,parent=None):
         QtGui.QWidget.__init__(self,parent)
         self.setupUi(self)
+        self.ButtonBack.clicked.connect(self.back1)
+        self.sim=0
         
+        self.ButtonOn.hide()
+        self.ButtonBack.hide()
+        self.ButtonPause.hide()
+        self.label_5.hide()
+
+        self.start.clicked.connect(self.start1)
+        self.back.clicked.connect(self.close)
+        self.ButtonOn.clicked.connect(self.on)
+        
+        self.ButtonPause.clicked.connect(self.pause)        
+        self.fig_dict={}
+        
+        self.slider_simulation.hide()
+#        self.horizontalSlider.valueChanged.connect(self.initial)
+        self.slider_simulation.valueChanged.connect(self.simulation)
+        self.fig_dict={}
+        self.mplfigs.itemClicked.connect(self.changefig)
+        self.fig=Figure()
+        self.addmpl(self.fig)
+
+
+    def start1(self):
+        self.sim=0
+        prevdir = os.getcwd()
+        try:
+            os.chdir(os.path.expanduser('./Wavepackdisper'))
+            file=open('input.txt','w')  
+            if (self.radioButton.isChecked()==True):
+                true=1
+            if (self.radioButton_2.isChecked()==True):
+                true=0
+            file.write ('%s\t%s\t%s' %(self.horizontalSlider.value(),true,self.spinBox.value()))
+            file.close()
+            subprocess.call('python gpe_fft_ts_WP_v1.py',shell=True)
+            print (os.getcwd())
+            print ("READY")
+            self.label_5.show()
+            self.ButtonOn.show()
+            self.ButtonBack.show()
+            self.ButtonPause.show()
+            self.label_5.show()
+            self.slider_simulation.show()
+            self.slider_simulation.setMinimum(0)
+            self.slider_simulation.setMaximum(self.spinBox.value()*int((10*np.pi*2.0))-1)
+            self.slider_simulation.setSingleStep(1)
+        
+            file = open('energies.txt', 'r')
+            lines = file.readlines()
+            file.close()
+
+        finally:
+            os.chdir(prevdir)
+                    
+        x1 = []
+        y1 = []
+        z1 = []
+        j1 = []
+        for line in lines:
+            p = line.split()
+            x1.append(float(p[0]))
+            y1.append(float(p[1]))
+            z1.append(float(p[3]))
+            j1.append(float(p[4]))                            
+        xv = np.array(x1)
+        yv = np.array(y1)
+        zv = np.array(z1)
+        jv=np.array(j1)
+        
+        fig=Figure()
+        ax1f2=fig.add_subplot(111)
+        ax1f2.set_xlabel('$T/t_{ho}$',fontsize=17)        
+        ax1f2.set_ylabel('$E/hw$',fontsize=17)
+        ax1f2.plot(xv,yv,label='$E_{tot}$')
+        ax1f2.plot(xv,zv,label='Kinetic Energy')
+        ax1f2.plot(xv,jv,label='Potential Energy')
+        ax1f2.set_title('Energies')
+    
+        
+        self.delfig()
+        self.addfig('ENERGY',fig)
+    
+    def simulation(self):
+        time=self.spinBox.value()*int((10*np.pi*2.0))-1
+        value=self.slider_simulation.value()
+        prevdir = os.getcwd()
+        try:
+            os.chdir(os.path.expanduser('./Wavepackdisper'))
+            for i in range(1,time+1):
+                file=open('WfWd-%08d.txt'%(i),'r')
+                globals()['lines%s' %i]=file.readlines()
+                file.close()
+#            
+#            
+                if value==i:                    
+                    x1=[]
+                    x2=[]
+                    x3=[]
+                    for line in (globals()['lines%s' %i]):
+                        p=line.split()
+                        x1.append(float(p[0]))
+                        x2.append(float(p[1]))
+                        x3.append(float(p[5]))
+                    xv1=np.array(x1)
+                    xv2=np.array(x2)
+                    xv3=np.array(x3)
+                    
+                    
+                    if self.fig==None:
+                        self.rmmpl()
+                        self.fig=Figure()
+                        self.addmpl(self.fig)
+                    self.fig.clear()
+                    axf=self.fig.add_subplot(111)
+                    axf.set_xlabel('$x/a_{ho}$',fontsize=17)
+                    axf.set_ylabel('density $|\psi|^2$',fontsize=14)
+                    axf.plot(xv1,xv2,label='R-Space')
+                    axf.plot(xv1,xv3,label='K-Space')
+                    axf.set_title('state at %s' %(i))
+                    self.canvas.draw()
+        finally:
+            os.chdir(prevdir)
+            
+    def plot(self):
+        prevdir = os.getcwd()
+        try:
+            os.chdir(os.path.expanduser('./Wavepackdisper'))
+            self.sim+=1
+            file=open('WfWd-%08d.txt'%(self.sim),'r')
+            globals()['lines%s' %self.sim]=file.readlines()
+            file.close()
+            x1=[]
+            x2=[]
+            x3=[]
+            for line in (globals()['lines%s' %self.sim]):
+                p=line.split()
+                x1.append(float(p[0]))
+                x2.append(float(p[1]))
+                x3.append(float(p[5]))
+            xv1=np.array(x1)
+            xv2=np.array(x2)
+            xv3=np.array(x3)
+        finally:
+            os.chdir(prevdir)
+        if self.fig==None:
+            self.rmmpl()
+            self.fig=Figure()
+            self.addmpl(self.fig)    
+        self.fig.clear()
+        axf=self.fig.add_subplot(111)
+        axf.set_xlabel('$x/a_{ho}$',fontsize=17)
+        axf.set_ylabel('density $|\psi|^2$',fontsize=14)
+        axf.plot(xv1,xv2,label="$R-Space$")
+        axf.plot(xv1,xv3,label='$K-Space$')
+        axf.set_title('state at %s' %(self.sim))
+        self.canvas.draw()
+        if (self.sim==self.spinBox.value()*int((10*np.pi*2.0)-1)):
+            self.timer.stop()    
+        
+    def plot2(self):
+        prevdir = os.getcwd()
+        try:
+            os.chdir(os.path.expanduser('./Wavepackdisper'))
+            self.sim-=1
+            file=open('WfWd-%08d.txt'%(self.sim),'r')
+            globals()['lines%s' %self.sim]=file.readlines()
+            file.close()
+            x1=[]
+            x2=[]
+            x3=[]
+            for line in (globals()['lines%s' %self.sim]):
+                p=line.split()
+                x1.append(float(p[0]))
+                x2.append(float(p[1]))
+                x3.append(float(p[5]))
+            xv1=np.array(x1)
+            xv2=np.array(x2)
+            xv3=np.array(x3)
+                
+        finally:
+            os.chdir(prevdir)
+        if self.fig==None:
+            self.rmmpl()
+            self.fig=Figure()
+            self.addmpl(self.fig)    
+        self.fig.clear()
+        axf=self.fig.add_subplot(111)
+        axf.set_xlabel('$x/a_{ho}$',fontsize=17)
+        axf.set_ylabel('density $|\psi|^2$',fontsize=14)
+        axf.plot(xv1,xv2,label='$R-Space$')
+        axf.plot(xv1,xv3,label='$K-Space$')
+        axf.set_title('state at %s' %(self.sim))
+        self.canvas.draw()
+        if (self.sim==1):
+            self.timer.stop()
+            
+    def on(self):
+        self.timer=QtCore.QTimer(self)
+        self.timer.timeout.connect(self.plot)
+        self.timer.start(250)
+    
+    def pause(self):
+        self.timer.stop()
+        
+    def back1(self):
+        self.timer.timeout.connect(self.plot2)
+        self.timer.start(75)
+        
+    def changefig(self,item):
+        text=item.text()
+        self.rmmpl()
+        self.addmpl(self.fig_dict[str(text)])
+        self.fig=None
+        
+    def addfig(self,name,fig):
+        self.fig_dict[name]=fig
+        self.mplfigs.addItem(name)
+        
+    def delfig(self):
+        listItems=self.mplfigs.selectedItems()
+        if not listItems: return
+        for item in listItems:
+            self.mplfigs.takeItem(self.mplfigs.row(item))
+            
+    def addmpl(self,fig):
+        self.canvas=FigureCanvas(fig)
+        self.toolbar=NavigationToolbar(self.canvas,self,coordinates=True)
+        self.mplvl.addWidget(self.toolbar)        
+        self.mplvl.addWidget(self.canvas)
+        self.canvas.draw()
+    
+    def rmmpl(self):
+        self.mplvl.removeWidget(self.toolbar)
+        self.canvas.close()        
+        self.mplvl.removeWidget(self.canvas)
+        self.canvas.close()
+        
+    def close(self):
+        self.hide()
+        self.parent().show()
         
     def closeEvent(self, event):
         reply = QtGui.QMessageBox.question(self, 'EXIT',
