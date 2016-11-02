f2e24f588e96eedc1348d4566c9a5671f67f3d33
Lauramova <95morenolaura@gmail.com>
2016-11-02 18:00:46 +0100

    Interfaz separada

---
+++ WD_interfaz.py
@@ -0,0 +1,476 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 30 16:02:58 2016
+
+@author: laura18
+"""
+
+import os
+import subprocess
+
+from PyQt4 import QtGui
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.uic import loadUiType
+import math
+import numpy as np
+
+from PyQt4 import QtGui, QtCore
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt4agg import (FigureCanvasQTAgg as FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+import time
+
+Ui_MainWindow,QMainWindow=loadUiType('WD.ui')
+class WD(QMainWindow,Ui_MainWindow):
+    def __init__(self,parent=None):
+        QtGui.QWidget.__init__(self,parent)
+        self.setupUi(self)
+        self.ButtonBack.clicked.connect(self.back1)
+        self.sim=0
+        self.true1=0
+        
+        self.ButtonOn.hide()
+        self.ButtonBack.hide()
+        self.ButtonPause.hide()
+        self.label_5.hide()
+        self.interact_game.hide()
+        self.game.hide()
+        
+        self.start.clicked.connect(self.start1)
+        self.back.clicked.connect(self.close)
+        self.ButtonOn.clicked.connect(self.on)
+        
+        self.ButtonPause.clicked.connect(self.pause)        
+        self.fig_dict={}
+        
+        self.slider_simulation.hide()
+#        self.horizontalSlider.valueChanged.connect(self.initial)
+        self.slider_simulation.valueChanged.connect(self.simulation)
+        self.fig_dict={}
+        self.mplfigs.itemClicked.connect(self.changefig)
+        self.fig=Figure()
+        self.addmpl(self.fig)
+        
+        self.pushButton_game.clicked.connect(self.game_call)
+        self.pushButton_try.clicked.connect(self.graph_try)
+        self.pushButton_return.clicked.connect(self.game_return)
+        self.mplwindow_2.hide()
+
+
+    def start1(self):
+        dialog = QtGui.QDialog()    
+        progressBar = Ui_porcessProgress()
+        progressBar.setupUi(dialog)
+        dialog.show()
+        diff = 0
+        self.sim=0
+        self.true1=0
+        prevdir = os.getcwd()
+        try:
+            os.chdir(os.path.expanduser('./Wavepackdisper'))
+            file=open('input.txt','w')  
+            if (self.radioButton.isChecked()==True):
+                self.true1=1
+            if (self.radioButton_2.isChecked()==True):
+                self.true1=0
+            file.write ('%s\t%s\t%s\t%s' %(self.horizontalSlider.value(),self.true1,self.spinBox.value(),self.spinBox_2.value()))
+            file.close()
+            
+            time1=self.spinBox.value()*int((10*np.pi*2.0))-1
+            subprocess.Popen('python gpe_fft_ts_WP_v1.py',shell=True)
+            for root, dirs, files in os.walk(os.getcwd()):
+                    for file in files:
+                        if file.startswith("WfWd"):
+                             os.remove((os.path.join(root, file)))
+                             
+            progressBar.porcessProgressBar.setMaximum(time1)
+            diff=0
+            while diff<time1+1:
+                diff=0
+                for root, dirs, files in os.walk(os.getcwd()):
+                    for file in files:
+                        if file.startswith("WfWd"):
+                            diff +=1
+                    
+                    if (diff<10):
+                        progressBar.label.setText('Initiation of the progress...')
+                    if (diff<time1-10) and (diff>10):
+                        progressBar.label.setText('Solving Schrodinger equation...')
+                    if (diff<time1) and (diff>time1-10):
+                        progressBar.label.setText('Writing results ...')
+                                            
+                    progressBar.porcessProgressBar.setValue(diff)                     
+                    QApplication.processEvents()
+                        
+            print (os.getcwd())
+            print ("READY")
+            self.label_5.show()
+            self.ButtonOn.show()
+            self.ButtonBack.show()
+            self.ButtonPause.show()
+            self.label_5.show()
+            self.slider_simulation.show()
+            self.slider_simulation.setMinimum(0)
+            self.slider_simulation.setMaximum(self.spinBox.value()*int((10*np.pi*2.0))-1)
+            self.slider_simulation.setSingleStep(1)
+            if (self.radioButton.isChecked()==True):
+                self.interact_game.show()
+            if (self.radioButton_2.isChecked()==True):
+                self.interact_game.hide()
+            time.sleep(2)
+            
+            file = open('energies.txt', 'r')
+            lines = file.readlines()
+            file.close()
+            
+            file2 = open('mean_value.txt','r')
+            lines2 = file2.readlines()
+            file2.close()
+            
+            file4=open('WfWd-%08d.txt'%(0),'r')
+            lines4=file4.readlines()
+            file4.close()
+
+        finally:
+            os.chdir(prevdir)
+            
+        x1=[]
+        x2=[]
+        x3=[]
+        for line in lines4:
+            p=line.split()
+            x1.append(float(p[0]))
+            x2.append(float(p[1]))
+            x3.append(float(p[5]))
+        xv1=np.array(x1)
+        xv2=np.array(x2)
+        xv3=np.array(x3)
+        
+        
+        self.rmmpl()
+        self.fig=Figure()
+        axf=self.fig.add_subplot(111)
+        axf.set_xlabel('$x/a_{ho}$',fontsize=17)
+        axf.set_ylabel('density $|\psi|^2$',fontsize=14)
+        axf.plot(xv1,xv2,label='R-Space')
+        axf.plot(xv1,xv3,label='K-Space')
+        axf.set_xlim([-20,20])
+        if (self.true1==0):
+            axf.set_ylim(0,0.6)
+        axf.set_title('state at %s' %(0))
+        axf.legend()
+        self.addmpl(self.fig)
+        
+ 
+        x1 = []
+        y1 = []
+        z1 = []
+        j1 = []
+        for line in lines:
+            p = line.split()
+            x1.append(float(p[0]))
+            y1.append(float(p[1]))
+            z1.append(float(p[3]))
+            j1.append(float(p[4]))                            
+        xv = np.array(x1)
+        yv = np.array(y1)
+        zv = np.array(z1)
+        jv=np.array(j1)
+        
+        fig=Figure()
+        ax1f1=fig.add_subplot(111)
+        ax1f1.set_xlabel('$T/t_{ho}$',fontsize=17)        
+        ax1f1.set_ylabel('$E/hw$',fontsize=17)
+        ax1f1.plot(xv,yv,'r.-',label='$E_{tot}$')
+        ax1f1.plot(xv,zv,'y.-',label='Kinetic Energy')
+        ax1f1.plot(xv,jv,'b.-',label='Potential Energy')
+        ax1f1.legend()
+        ax1f1.set_title('Energies')
+        
+        
+    
+        x1 = []
+        y1 = []
+        z1 = []
+        i1 = []
+        j1 = []
+        for line in lines2:
+            p = line.split()
+            x1.append(float(p[0]))
+            y1.append(float(p[1]))
+            z1.append(float(p[2]))  
+            i1.append(float(p[3]))
+            j1.append(float(p[4]))                          
+        xv = np.array(x1)
+        yv = np.array(y1)
+        zv = np.array(z1)
+        iv = np.array(i1)
+        jv = np.array(j1)
+        
+        self.fig2=Figure()
+        ax1f2=self.fig2.add_subplot(111)
+        ax1f2.set_xlabel('$T/t_{ho}$',fontsize=17)        
+        ax1f2.set_ylabel('$x/a_{ho}$',fontsize=17)
+        ax1f2.plot(xv,yv, 'b.-',label='$R-Space$')
+        ax1f2.plot(xv,iv, 'y.-',label='$k-Space$')
+        ax1f2.legend()
+        ax1f2.set_title('Mean value x')
+        
+        fig3=Figure()
+        ax2f3=fig3.add_subplot(111)
+        ax2f3.set_xlabel('$T/t_{ho}$',fontsize=17)
+        ax2f3.set_ylabel('$x/a_{ho}$',fontsize=17)
+        ax2f3.plot(xv,zv, 'b.-',label='$R-Space$')
+        ax2f3.plot(xv,jv, 'y.-',label='$k-Space$')
+        ax2f3.legend()
+        ax2f3.set_title('Mean value x dispersion')
+
+        self.delfig()        
+        self.delfig()
+        self.delfig()
+        self.addfig('ENERGY',fig)
+        self.addfig('MEAN VALUE X',self.fig2)
+        self.addfig('DISPERSION', fig3)
+        self.slider_simulation.setValue(0)
+        
+    def simulation(self):
+        time1=self.spinBox.value()*int((10*np.pi*2.0))-1
+        value=self.slider_simulation.value()
+        self.sim=value
+        prevdir = os.getcwd()
+        try:
+            os.chdir(os.path.expanduser('./Wavepackdisper'))
+            for i in range(0,time1+1):
+                file=open('WfWd-%08d.txt'%(i),'r')
+                globals()['lines%s' %i]=file.readlines()
+                file.close()
+#            
+#            
+                if value==i:                    
+                    x1=[]
+                    x2=[]
+                    x3=[]
+                    for line in (globals()['lines%s' %i]):
+                        p=line.split()
+                        x1.append(float(p[0]))
+                        x2.append(float(p[1]))
+                        x3.append(float(p[5]))
+                    xv1=np.array(x1)
+                    xv2=np.array(x2)
+                    xv3=np.array(x3)
+                    
+                    
+                    if self.fig==None:
+                        self.rmmpl()
+                        self.fig=Figure()
+                        self.addmpl(self.fig)
+                    self.fig.clear()
+                    axf=self.fig.add_subplot(111)
+                    axf.set_xlabel('$x/a_{ho}$',fontsize=17)
+                    axf.set_ylabel('density $|\psi|^2$',fontsize=14)
+                    axf.plot(xv1,xv2,label='R-Space')
+                    axf.plot(xv1,xv3,label='K-Space')
+                    axf.set_xlim([-20,20])
+                    if (self.true1==0):
+                        axf.set_ylim(0,0.6)
+                    axf.set_title('state at %s' %(i))
+                    axf.legend()
+                    self.canvas.draw()
+        finally:
+            os.chdir(prevdir)
+    
+    def game_call(self):
+        if (self.radioButton.isChecked()==True):
+            
+            if (self.radioButton_oscil.isChecked()==True):
+                self.rmmpl()
+                self.textBrowser.show()
+                self.rmmpl2()
+                self.mplwindow.hide()
+                self.widget_osci.show()
+                self.start.hide()
+                self.mplfigs.hide()
+                prevdir = os.getcwd()
+                try:
+                    os.chdir(os.path.expanduser('./Wavepackdisper'))
+                    file2 = open('mean_value.txt','r')
+                    lines2 = file2.readlines()
+                    file2.close()
+                    x1 = []
+                    y1 = []
+                    for line in lines2:
+                        p = line.split()
+                        x1.append(float(p[0]))
+                        y1.append(float(p[1]))                        
+                    xv = np.array(x1)
+                    yv = np.array(y1)
+                finally:
+                    os.chdir(prevdir)
+                fig3=Figure()
+                ax1f3=fig3.add_subplot(111)
+                ax1f3.set_xlabel('$T/t_{ho}$',fontsize=17)        
+                ax1f3.set_ylabel('$x/a_{ho}$',fontsize=17)
+                ax1f3.plot(xv,yv, 'r.-',label='$R-Space$')                
+                ax1f3.set_title('Mean value x')
+                ax1f3.legend()
+                self.addmpl2(fig3)
+
+        self.game.show()
+        self.mplwindow_2.show()
+        
+    def graph_try(self):
+        if (self.radioButton.isChecked()==True):
+            if (self.radioButton_oscil.isChecked()==True):
+                time1=self.spinBox.value()*int((10*np.pi*2.0))-1
+                self.rmmpl2()
+                prevdir = os.getcwd()
+                try:
+                    os.chdir(os.path.expanduser('./Wavepackdisper'))
+                    file2 = open('mean_value.txt','r')
+                    lines2 = file2.readlines()
+                    file2.close()
+                    x1 = []
+                    y1 = []
+                    for line in lines2:
+                        p = line.split()
+                        x1.append(float(p[0]))
+                        y1.append(float(p[1]))                        
+                    xv = np.array(x1)
+                    yv = np.array(y1)
+                finally:
+                    os.chdir(prevdir)
+                
+                fig3=Figure()
+                self.addmpl2(fig3)
+                ax1f3=fig3.add_subplot(111)
+                ax1f3.set_xlabel('$T/t_{ho}$',fontsize=17)        
+                ax1f3.set_ylabel('$x/a_{ho}$',fontsize=17)
+                
+                x=np.arange(0,time1+1)/10.
+                y=self.spin_amplitude.value()*np.cos(self.spin_frequency.value()*x)
+                
+                ax1f3.plot(xv,yv, 'r.-',label='$R-Space$')                
+                ax1f3.plot(x,y)            
+                ax1f3.set_title('Mean value x')
+                ax1f3.legend()
+                self.canvas.draw()
+                
+    def game_return(self):
+        self.rmmpl2()
+        self.rmmpl2()
+        self.mplwindow_2.hide()
+        self.game.hide()
+        self.fig=Figure()
+        self.addmpl(self.fig)
+        self.mplwindow.show()
+        self.start.show()
+        self.mplfigs.show()
+
+        
+    def plot(self):
+        self.sim +=1
+        self.slider_simulation.setValue(self.sim)
+        
+        
+        if (self.sim==self.spinBox.value()*int((10*np.pi*(2.))-1)):
+            self.sim=self.sim-1
+            self.timer.stop()
+        
+    def plot2(self):
+        self.sim -=1
+        self.slider_simulation.setValue(self.sim)
+
+        
+        if (self.sim==0):
+            self.timer.stop()
+            
+    def on(self):
+        self.timer=QtCore.QTimer(self)
+        self.timer.timeout.connect(self.plot)
+        self.timer.start(75)
+    
+    def pause(self):
+        self.timer.stop()
+        
+    def back1(self):
+        self.timer=QtCore.QTimer(self)
+        self.timer.timeout.connect(self.plot2)
+        self.timer.start(75)
+        
+    def changefig(self,item):
+        text=item.text()
+        self.rmmpl()
+        self.addmpl(self.fig_dict[str(text)])
+        self.fig=None
+        
+    def addfig(self,name,fig):
+        self.fig_dict[name]=fig
+        self.mplfigs.addItem(name)
+        
+    def delfig(self):
+        listItems=self.mplfigs.selectedItems()
+        if not listItems: return
+        for item in listItems:
+            self.mplfigs.takeItem(self.mplfigs.row(item))
+            
+    def addmpl(self,fig):
+        self.canvas=FigureCanvas(fig)
+        self.toolbar=NavigationToolbar(self.canvas,self,coordinates=True)
+        self.mplvl.addWidget(self.toolbar)        
+        self.mplvl.addWidget(self.canvas)
+        self.canvas.draw()
+    
+    def rmmpl(self):
+        self.mplvl.removeWidget(self.toolbar)
+        self.canvas.close()        
+        self.mplvl.removeWidget(self.canvas)
+        self.canvas.close()
+        
+    def addmpl2(self,fig):
+        self.canvas=FigureCanvas(fig)
+        self.toolbar=NavigationToolbar(self.canvas,self,coordinates=True)
+        self.mplvl_2.addWidget(self.toolbar)        
+        self.mplvl_2.addWidget(self.canvas)
+        self.canvas.draw()
+        
+    def rmmpl2(self):
+        self.mplvl_2.removeWidget(self.toolbar)
+        self.canvas.close()        
+        self.mplvl_2.removeWidget(self.canvas)
+        self.canvas.close()
+        
+    def close(self):
+        self.hide()
+        self.parent().show()
+        
+    def closeEvent(self, event):
+        reply = QtGui.QMessageBox.question(self, 'EXIT',
+            "Are you sure to quit?", QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
+
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+            
+class Ui_porcessProgress(object):
+    def setupUi(self, porcessProgress):
+        porcessProgress.setObjectName("porcessProgress")
+        porcessProgress.setWindowModality(QtCore.Qt.ApplicationModal)
+        porcessProgress.resize(329, 81)
+        porcessProgress.setMinimumSize(QtCore.QSize(329, 81))
+        porcessProgress.setMaximumSize(QtCore.QSize(329, 81))
+        porcessProgress.setModal(True)
+        self.verticalLayout = QtGui.QVBoxLayout(porcessProgress)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtGui.QLabel(porcessProgress)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.porcessProgressBar = QtGui.QProgressBar(porcessProgress)
+        self.porcessProgressBar.setMaximum(10)
+        self.porcessProgressBar.setProperty("value", 10)
+        self.porcessProgressBar.setTextVisible(False)
+        self.porcessProgressBar.setObjectName("porcessProgressBar")
+        self.verticalLayout.addWidget(self.porcessProgressBar)
+        porcessProgress.setWindowTitle(QtGui.QApplication.translate("porcessProgress", "Please wait...", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("porcessProgress", " in progress...", None, QtGui.QApplication.UnicodeUTF8))
+        QtCore.QMetaObject.connectSlotsByName(porcessProgress)
