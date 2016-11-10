9ff09c1f5b8c36b825498fcb3e17ca6d777dd59a
Lauramova <95morenolaura@gmail.com>
2016-11-10 18:33:53 +0100

    código bs
    Subo los archivos cada vez por posibles pequeñas modificaciones que haya hecho

---
+++ gpe_bs_parameters.py
@@ -0,0 +1,139 @@
+# coding: utf-8
+import numpy as np
+from gpe_bs_utilities import *
+import math
+pi=np.pi
+
+
+# Data block
+# ------------------------------------------------------------------------------
+#importing data file with input parameters
+infile=open('input.txt', 'r')
+listin=infile.readlines()
+values=listin[0].split(' \t')    #read the line with inputs, only contains one line
+infile.close()
+
+V_ext=int(values[0])
+if V_ext==0:    #no external potential    
+    x0=float(values[2])
+    int_str=int(values[1])
+    velocity=float(values[3])
+    def_w=int(values[4])
+    simtime=float(values[5])
+#    print V_ext,int_str,x0,velocity,def_w  #to check if it works or not
+elif V_ext==1:  #harmonic trap
+    int_str=int(values[1])
+    x0=float(values[2])
+    oscil=int(values[3])
+    def_w=int(values[4])
+#    print V_ext,int_str,x0,oscil,def_w
+elif V_ext==2:  #wall
+    int_str=int(values[1])
+    x0=float(values[2])
+    velocity=float(values[3])
+    def_w=int(values[4])
+    barrier_w=int(values[5])
+    barrier_h=float(values[6])
+    simtime=float(values[7])
+#    print V_ext,int_str,x0,velocity,def_w,barrier_w,barrier_h
+    
+if int_str == 1:
+    a_s = -0.005e-3
+elif int_str == 2:
+    a_s = -0.01e-3
+elif int_str == 3:
+    a_s = -0.02e-3
+
+
+whoz = 1.0                          # harmonic oscilator angular frequency with f=2*pi
+freq = 2.0*np.pi/whoz               #frequency
+
+if V_ext==1:
+    Zmax=2.0**5
+    time_final=float(oscil)*freq
+    Dti=1.0e-1                      # imaginary time step (not used)
+    Ntime_fin=int(3.0e3*oscil)            # supposed as in the original one for total time=20 and Dtr=10^-3
+    Dtr=float(time_final/float(Ntime_fin))
+  #  Dtr=2.0e-3
+  #  Ntime_fin=int(time_final/Dtr)
+    Npoint = 2**10                      # Number of grid points (min. 2**8)
+    velocity=0.0
+    Ntime_out = 50                     # number of time steps for intermediate outputs
+elif V_ext==0:
+    Zmax = 2.0**7    
+    Dtr = 2.0e-3                        # real time step (min. 1.0e-2)
+    Dti = 1.0e-1                        # imaginary time step
+    time_final = simtime                   # final time
+    Ntime_fin = int(time_final/Dtr)     # total number of time steps
+    Npoint = 2**10                      # Number of grid points (min. 2**8)
+    oscil=0                             # Set number of oscillations to zero, no harmonic potential
+    Ntime_out = 100*simtime//20                      # number of time steps for intermediate outputs
+elif V_ext==2:
+    Zmax = 2.0**7    
+    Dtr = 2.0e-3                        # real time step (min. 1.0e-2)
+    Dti = 1.0e-1                        # imaginary time step
+    time_final = simtime*float(math.ceil(-2.0*x0/velocity))  # final time
+    Ntime_fin = int(time_final/Dtr)     # total number of time steps
+    Npoint = 2**10                      # Number of grid points (min. 2**8)
+    oscil=0                             # set number of oscillations to zero, no harmonic potential
+    Ntime_out = 100*int(simtime)        # number of time steps for intermediate outputs
+    
+#Zmax = 2.0**7                       # Grid half length
+Nparticle = 20e+3                   # Number of particles
+
+# Derived quantities and parameters of the initial wavefunction (bright soliton)
+# ------------------------------------------------------------------------------
+
+NormWF = 1.0/(2*Zmax)               # Wave function (WF) norm
+Dz = 2*Zmax/Npoint                  # length step size
+Dk = pi/Zmax                        # momentum step size
+Kmax = Dk*(Npoint//2)               # maximum momentum
+Dt = Dtr-1j*Dti                     # complex time
+Ninter = Ntime_fin//Ntime_out       # Number of outputs with the intermediate states
+gn = 2*a_s*Nparticle                # interaction (nonlinear-term) strength
+gint = gn*NormWF                    # int. strenght, with factor NormWF
+xi = 1.0/(np.abs(gn)**2*0.5)**0.5   # healing length
+v = float(velocity)                 # initial velocity of the soliton
+
+
+# Potential walls and barrier
+# ------------------------------------------------------------------------------
+
+if def_w == 1:
+    wall = 1
+elif def_w == 0:
+    wall = 0
+
+wall_h = 100.0                      # height of the walls
+
+
+def height_barrier(a):
+    """Height of the barrier, defined as 'a' times the initial kinetic energy of the soliton
+    with initial velocity 'v', defined as Ekin = 0.5 * v**2.
+    Returns Ekin * a.
+    """
+    global v
+    height = a * v**2 * 0.5
+    return height
+
+# potential barrier
+
+if V_ext == 2:
+
+    xb = 0.0*Zmax                       # position of the potential barrier
+
+#    if barrier_w == 1:
+#        wb = 0.5 * xi
+#    elif barrier_w == 2:
+#        wb = 1.0 * xi
+#    elif barrier_w == 3:
+#        wb = 2.0 * xi
+    wb=barrier_w*xi  #with the new input, barrier_W has already the correct value (0.5, 1.0 or 2.0)
+
+    xbr = xb + 0.5*wb                   # position of the right wall (barrier)
+    xbl = xb - 0.5*wb                   # positon of the left wall (barrier)
+
+    hb = height_barrier(barrier_h)
+
+else:
+    xb, hb, wb = 0, 0, 0
---
+++ gpe_bs_utilities.py
@@ -0,0 +1,211 @@
+# coding: utf-8
+import numpy as np
+import numpy.linalg as lin
+from scipy.fftpack import fft, ifft
+from sys import exit
+from gpe_bs_parameters import *
+
+
+# Initial wavefunction (bright soliton) and potential
+# ------------------------------------------------------------------------------
+
+def bright_soliton(x,n,x0,gn):
+    """Bright soliton: 1/cosh(x/xi) wf, analytic solution
+       x  = grid points
+       n  = number of grid points
+       x0 = soliton position
+       gn = (negative) interaction energy or double of the chemical potential
+    """
+    if (gn >= 0):
+        exit(" error: interaction must be negative...")
+    psi0 = 0.5*np.sqrt(np.abs(gn))
+    xi = 1.0/(2.0*np.abs(gn)*np.abs(psi0)**2)**0.5
+    fx = psi0 * 1.0/np.cosh((x-x0)/(xi * np.sqrt(2.0)))   # wave function in R3
+    return fft(fx)/n    # FFT to K3
+
+def Vpot(n, z):
+    """External potential according to n. Cases:
+       n=0     no potential
+       n=1     harmonic trap
+       n=2     square potential barrier
+    The walls of the box containing the soliton are defined allways unless
+    the height of the walls is set to zero.
+    Parameters (introduced as arguments):
+       z       R-space grid points
+    Global variables:
+       Zmax    halfwidth of the grid
+       Npoint  number of grid points
+       whoz    harmonic oscillator frequency
+       xb      position of the barrier
+       xbr     position of the right wall (barrier)
+       xbl     position of the left wall (barrier)
+       hb      height of the barrier
+       wall    defines potential walls if not 0
+    """
+    global Zmax, Npoint, whoz, xb, xbr, xbl, hb, wall, wall_h
+    Vpot_R = np.empty([Npoint]) # vector of the same length as z, initially empty
+    Vpot_Rc = np.empty([Npoint])#confinement
+    m = 1000                    # slope used to define the walls and barrier
+    wall_x = 0.75*Zmax          # position of the walls relative to the halfwidth of the box
+    tol_exp = 1e100; tol_arg = 500
+    # defines the walls of the box
+    if wall != 0 :
+        Vpot_Rc = 0.0*z
+        for i in range(0,Npoint):
+            if m*(z[i]-wall_x) > tol_arg:
+                wall_exp1 = tol_exp
+            else:
+                wall_exp1 = np.exp(m*(z[i]-wall_x))
+            if m*(z[i]+wall_x) > tol_arg:
+                wall_exp2 = tol_exp
+            else:
+                wall_exp2 = np.exp(m*(z[i]+wall_x))
+            Vpot_Rc[i] = wall_h - wall_h/(1.0+wall_exp1) + wall_h/(1.0+wall_exp2)
+    else:
+        Vpot_Rc = 0.0*z
+    # defines the potential (barriers, harmonic trap, etc.)
+    if(n==0):
+        Vpot_R = Vpot_Rc
+    elif(n==1):
+#        Vpot_R = 0.5*(whoz**2)*(z**2) + Vpot_Rc
+        Vpot_R = 0.5*(whoz**2)*(z**2)
+    elif(n==2):
+        Vpot_R = 0.0*z
+        Vpot_0 = 0.0*z
+        for i in range(0,Npoint):
+            if m*(z[i]-xbr) > tol_arg:
+                barrier_exp1 = tol_exp
+            else:
+                barrier_exp1 = np.exp(m*(z[i]-xbr))
+            if m*(z[i]-xbl) > tol_arg:
+                barrier_exp2 = tol_exp
+            else:
+                barrier_exp2 = np.exp(m*(z[i]-xbl))
+            Vpot_R[i] = Vpot_0[i] + hb/(1.0+barrier_exp1) - hb/(1.0+barrier_exp2)
+    else:
+        Vpot_R = Vpot_Rc
+    return Vpot_R,Vpot_Rc
+
+
+# operators and energies
+# ------------------------------------------------------------------------------
+
+def Energy(c, Vpot_R, Ekin_K):
+    """Energy (per particle) calculation for a certain time.
+       c         initial wavefunction (FFT ordered)
+       Vpot_R    potential as function of the position
+       Ekin_K    kinetic energy as function of the position
+    Global variables:
+       gint      interaction strength * NormWF (norm)
+       Npoint    number of points in the grid
+    Energy terms:
+       ek        kinetic energy
+       ep        potential energy
+       ei        interaction energy
+       em        average energy
+       chem_pot  chemical potential
+    Returns the energy terms.
+    """
+    global gint, Npoint
+    ek = sum(Ekin_K*abs(c)**2)              # Kinetic energy in K
+    psi = ifft(c)*Npoint;                   # wf FFT to R
+    ep = sum(Vpot_R*abs(psi)**2)/Npoint;
+    ei = 0.5*gint*sum(abs(psi)**4)/Npoint;
+    em =  ek+ep+ei;
+    chem_pot = em+ei;
+    return em, chem_pot, ek, ep, ei
+    
+def velmean(c,Vpot_R,Ekin_K):
+    global Dk
+    vmean=sum(np.sqrt(2*Ekin_K)*abs(c)**2) 
+    return vmean
+
+def T_K(Dt, Ekin_K):
+    """Time evolution operator in K space (for second order accuracy).
+       Dt      complex time step
+       Ekin_K  kinetic energy (function of the position)
+    """
+    T_K = np.exp(-1j*0.5*Dt*Ekin_K)
+    return T_K
+
+def T_R_psi(t, Dt, psi, Vpot_R):
+    """Action of the time evolution operator over state c in R space
+       t       time (not used for a time independent Hamiltonian)
+       Dt      complex time step
+       psi     wavefunction in R space
+       Vpot_R  external potential
+    Global variables:
+       gint    interaction strength * NormWF (norm)
+    Includes the external potential and the interaction operators:
+       T_R_psi = exp(-i Dt (Vpot_R+ gint|psi|^2) ) c
+    Returns the action on psi.
+    """
+    global gint
+    return np.exp( -1j*Dt*(Vpot_R + gint*(abs(psi)**2)) )*psi
+
+
+
+# Other utilities
+# ------------------------------------------------------------------------------
+
+def normaliza(c,file):
+    """Checks normalization of the wavefunction c and sets norm to 1.
+    Norm is written on a file unless variable 'file' is either 0 or 1."""
+    frmt = "%.10f \n"
+    norm = lin.norm(c)
+    if file != 0 and file != 1: #and != 1:
+        file.write(frmt %(norm))
+    elif file == 1:
+        pass
+    return c/norm
+
+def changeFFTposition(f,N,j):
+    """Change the order in vectors from FFT
+       f(0...N-1) is the vector to order
+       N is the vector dimension
+       j is a switch indicating the change direction
+       physical order is f = [(-(Zmax-Dz):Dz:-Dz) (0:Dz:Zmax)]
+       FFT order is f = [(0:Dk:kmax) (-(kmax-kz):kz:-kz)]
+    """
+    f1 = f*1
+    if (j==1):  # from physical to FFT order
+        for i in range(0,N//2-1) :
+            f1[i] = f[N//2-1+i];
+            f1[N//2+1+i] = f[i];
+        f1[N//2-1] = f[N-2];
+        f1[N//2] = f[N-1];
+    elif (j==0): # from FFT to physical order
+        for i in range(0,N//2-1) :
+            f1[i] = f[N//2+1+i];
+            f1[N//2+1+i] = f[i+2];
+        f1[N//2-1] = f[0];
+        f1[N//2] = f[1];
+    else:
+        print("error in changeFFTposition(f,N,j): j must be 0 or 1...")
+    return f1
+
+def integral(x,dz,z,i,f):
+    """Integrates function "x" from i to f.
+    x is a function (vector)
+    dz is the length step size
+    z is the grid
+    i is the initial piont
+    f is the last point
+    k, j are counters
+    """
+    k = 0
+    if len(x) != len(z):
+        print("Error: the function has not the same length as the grid")
+    inte = 0.0
+    for j in z:
+        if j>=i and j<=f:
+            inte += x[k]*dz
+        k += 1
+    return inte
+
+def list_integrals(x,z):
+    global Dz, Zmax, xbl, xbr
+    ileft = integral(x,Dz,z,-Zmax,xbl)
+    iin = integral(x,Dz,z,xbl,xbr)
+    iright = integral(x,Dz,z,xbr,Zmax)
+    return ileft, iin, iright
